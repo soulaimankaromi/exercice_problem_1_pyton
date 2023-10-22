14dfff07b7b4213d8bb3d2d6818a25404a35f69b
SOULAIMANKAROMI <147172168+soulaimankaromi@users.noreply.github.com>
2023-10-23 00:46:56 +0100

    Add files via upload

--- algorithme problem_1.py
+++ algorithme problem_1.py
@@ -14,9 +14,11 @@ pour i <= 0 an n1 pas 1 faire:
 ecrire("Donnez le prix de l'article ",i+1," :")
 lire(a)
     tab1[i] <= a
+finpour
 pour i <= 0 an n1 pas 1 faire
     t1<=tab1[i]*(1+0.15-0.02)
     s1<=s1+t1
+finpour
 ecrire("Donner le nom et prénom du client 2 :")
 lire(NP2)
 ecrire("Donner le nombre darticle pour le client 2 :")
@@ -27,9 +29,11 @@ pour j <= 0 an n2 pas 1 faire:
 ecrire("Donnez le prix de l'article ",j+1," :")
 lire(b)
     tab2[j] <= a
+finpour
 pour j <= 0 an n2 pas 1 faire
     t2<=tab2[j]*(1+0.15-0.02)
     s2<=s2+t2
+finpour
 ecrire("Donner le nom et prénom du client 3 :")
 lire(NP3)
 ecrire("Donner le nombre darticle pour le client 3 :")
@@ -40,10 +44,13 @@ pour g <= 0 an n3 pas 1 faire:
 ecrire("Donnez le prix de l'article ",g+1," :")
 lire(c)
     tab2[g] <= c
+finpour
 pour g <= 0 an n3 pas 1 faire
     t3<=tab3[g]*(1+0.15-0.02)
     s3<=s3+t3
+finpour
 ecrire("Facture est : ")    
 ecrire("Le Total à payer pour le client",NP1," est :",s1,"DH")
 ecrire("Le Total à payer pour le client",NP2," est :",s2,"DH")
 ecrire("Le Total à payer pour le client",NP3," est :",s3,"DH")
+fin
